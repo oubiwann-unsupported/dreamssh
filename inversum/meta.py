508a56c0a0ade8ce25c847de9cc4a37bf2adec79
Duncan McGreggor <duncan@dreamhost.com>
2012-05-05 01:07:23 -0400

    Added a meta file.

---
+++ inversum/meta.py
@@ -0,0 +1,12 @@
+display_name = "inversum"
+library_name = "inversum"
+version = "0.1"
+author = "Duncan McGreggor"
+author_email = "dev-community@dreamhost.com"
+license = "BSD"
+url = "http://github.com/dreamhost/inversum-aetatis"
+description = "Inversum Aetatis"
+long_description = """Inversum Aetatis"""
+install_requires = [
+    "twisted",
+    ]
