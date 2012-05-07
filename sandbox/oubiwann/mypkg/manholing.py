51a8a04e88897ebe3e800d296125a5a9b66861ac
Duncan McGreggor <duncan@dreamhost.com>
2012-05-07 01:35:57 -0400

    Found the source of the problem!

--- sandbox/oubiwann/mypkg/manholing.py
+++ sandbox/oubiwann/mypkg/manholing.py
@@ -1,5 +1,7 @@
+from twisted.conch.insults.insults import ServerProtocol
 from twisted.conch.manhole import Manhole
 from twisted.conch.manhole_tap import *
+from twisted.conch.recvline import RecvLine
 
 
 connectionMadeOrig = Manhole.connectionMade
@@ -10,4 +12,10 @@ def connectionMade(self, *args, **kwargs):
     self.terminal.write("This is your MOTD from the Manhole monkeypatch!")
 
 
+ServerProtocol.reset = lambda self: None
+RecvLine.initializeScreen = lambda self: None # <-- this is the cuplrit!
+# In order to get a MOTD, you need to override
+# manhole_ssh.TerminalSession.openShell, and do the MOTD after the upcall to
+# openShell.
+RecvLine.terminalSize = lambda self, w, h: None
 Manhole.connectionMade = connectionMade
