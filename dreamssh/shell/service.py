d6aeed9e6a37cda84f24983f210693ed3485ddda
Duncan McGreggor <duncan@dreamhost.com>
2012-05-10 00:18:31 -0400

    Service cleanup.

--- dreamssh/shell/service.py
+++ dreamssh/shell/service.py
@@ -2,26 +2,27 @@ from twisted.cred import portal
 from twisted.conch import manhole_ssh
 from twisted.conch.checkers import SSHPublicKeyDatabase
 
-from dreamssh import util
+from dreamssh import const, util
 from dreamssh.shell import base, pythonshell
 
 
-def realmFactory(interpreterType, namespace):
-    if interpreterType == base.PYTHON:
+def portalFactory(interpreterType, namespace):
+    if interpreterType == const.PYTHON:
+
+        def getManhole(serverProtocol):
+            commandAPI = pythonshell.CommandAPI()
+            return pythonshell.PythonManhole(commandAPI, namespace)
+
+        realm = pythonshell.PythonTerminalRealm(namespace)
+        realm.chainedProtocolFactory.protocolFactory = getManhole
+
+    elif interpreterType == const.ECHO:
         pass
-    elif interpreterType == base.ECHO:
-        pass
+    return portal.Portal(realm)
 
 
 def getShellFactory(interpreterType, **namespace):
-
-    def getManhole(serverProtocol):
-        commandAPI = pythonshell.CommandAPI()
-        return pythonshell.PythonManhole(commandAPI, namespace)
-
-    realm = pythonshell.PythonTerminalRealm(namespace)
-    realm.chainedProtocolFactory.protocolFactory = getManhole
-    sshPortal = portal.Portal(realm)
+    sshPortal = portalFactory(interpreterType, namespace)
     factory = manhole_ssh.ConchFactory(sshPortal)
     factory.privateKeys = {'ssh-rsa': util.getPrivKey()}
     factory.publicKeys = {'ssh-rsa': util.getPubKey()}
