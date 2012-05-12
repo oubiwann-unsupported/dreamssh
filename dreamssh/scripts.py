dbf8945ac47d70b90c482a0b3b1e53d20122041c
Duncan McGreggor <duncan@dreamhost.com>
2012-05-12 00:35:48 -0400

    Added classes that represent the logic for various scripts.

---
+++ dreamssh/scripts.py
@@ -0,0 +1,53 @@
+import os
+import subprocess
+
+from twisted.conch.scripts import ckeygen
+
+from dreamssh import config
+
+
+class Script(object):
+    """
+    """
+    def __init__(self):
+        self.run()
+
+    def run(self):
+        raise NotImplementedError()
+
+
+class KeyGen(Script):
+    """
+    """
+    def run(self):
+        path = config.ssh.keydir
+        key = os.path.join(path, "id_rsa")
+        if not os.path.exists(path):
+            print "Creating SSH key dir '%s' ..." % path
+            os.makedirs(path)
+        else:
+            print "SSH key dir '%s' already exists." % path
+        if not os.path.exists(key):
+            print "Creating SSH key at '%s' ..." % key
+            print "  (This could take a while)"
+            options = {"filename": key, "bits": 4096, "pass": None}
+            ckeygen.generateRSAkey(options)
+        else:
+            print "SSH key '%s' already exists." % key
+
+
+class ConnectToShell(Script):
+    """
+    """
+    def run(self):
+        print "Connecting to DreamSSH Server ..."
+        subprocess.call(["ssh",  "-p %s" % config.ssh.port,  "127.0.0.1"])
+
+
+class StopDaemon(Script):
+    """
+    """
+    def run(self):
+        print "Stopping DreamSSH Server ..."
+        pid = open("twistd.pid").read()
+        subprocess.call(["kill", pid])
