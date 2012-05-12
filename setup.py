f5d2ef1b3e37f19564e958edbe886fdccdef310f
Duncan McGreggor <duncan@dreamhost.com>
2012-05-11 20:31:34 -0400

    Added a setup.py file.

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+from dreamssh import meta
+
+
+setup(
+    name=meta.display_name,
+    version=meta.version,
+    description=meta.description,
+    author=meta.author,
+    author_email=meta.author_email,
+    url=meta.url,
+    license=meta.license,
+    packages=find_packages(),
+    long_description=meta.long_description,
+    install_requiers=meta.requires
+    )
