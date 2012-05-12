903b3c80f2958434aa9f7c093989b753172d308e
Duncan McGreggor <duncan@dreamhost.com>
2012-05-12 00:37:45 -0400

    Added more constants.

--- dreamssh/const.py
+++ dreamssh/const.py
@@ -1,3 +1,9 @@
 # Interpreter types
 PYTHON = "python"
 ECHO = "echo"
+
+# Prevent use of raw strings when we can help it
+INTERPRETER = "interpreter"
+KEYGEN = "keygen"
+SHELL = "shell"
+STOP = "stop"
